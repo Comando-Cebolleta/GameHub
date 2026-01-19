38c161ddf296f569409366d712d6fb6d14fe5f4e
irove04 <maresctor4@alu.edu.gva.es>
2026-01-19 09:30:43 +0100

    login y register con purpurina (he quitado lo de agree terms en register)

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -19,6 +19,7 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
+            /*
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -26,7 +27,9 @@ class RegistrationFormType extends AbstractType
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
+
             ])
+            */
             ->add('userName')
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
