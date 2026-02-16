f3b6a69fbcfb7a47d8e89beca70ba704e9b7fee3
irove04 <maresctor4@alu.edu.gva.es>
2026-02-16 21:38:15 +0100

    quitado js añadido slider

---
+++ public/js/sliderBlog.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    //aqui guardamos el carrusel en una variable
+    var $carrusel = $('.mi-carrusel');
+
+    // 1. Inicializamos Slick
+    $carrusel.slick({
+        dots: false,
+        autoplay: false,
+        infinite: false,
+        adaptiveHeight: true,
+        arrows: true,
+        prevArrow: $('.btn-prev'),
+        nextArrow: $('.btn-next')
+    });
+
+    //evento para cambiar el título (H1) cuando el carrusel se mueve
+    $carrusel.on('afterChange', function(event, slick, currentSlide){
+        //currentSlide es 0 para Posts y 1 para Builds
+        if (currentSlide === 0) {
+            $('#carousel-title').text('Publicaciones');
+        } else {
+            $('#carousel-title').text('Builds de la Comunidad');
+        }
+    });
+});
